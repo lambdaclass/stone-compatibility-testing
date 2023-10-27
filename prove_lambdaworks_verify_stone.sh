475416a3efda462a946e2a391d8eb584289f6a2f
MauroFab <maurotoscano2@gmail.com>
2023-10-27 18:31:02 -0300

    Fix format

--- prove_lambdaworks_verify_stone.sh
+++ prove_lambdaworks_verify_stone.sh
@@ -2,10 +2,10 @@
 
 # Prove a Fibonacci example using Lambdaworks. This produces a file `lambdaworks_proof.bin`
 # containing the proof in the format expected by Stone.
-echo -e "Generating proof with Lambdaworks Stark Platinum ...\n"
+echo -e "Generating proof with Lambdaworks Stark Platinum ..."
 /app/lambdaworks_prover/target/release/lambdaworks_prover
-echo -e "Proof generated!"
+echo -e "Proof generated!\n"
 
-echo -e "Verifying proof with Stone ...\n"
+echo -e "Verifying proof with Stone ..."
 # Read proof from file `lambdaworks_proof.bin` and verify it using Stone.
 /app/build/Release/fibonacci_air_test_cases/verify_from_bytes lambdaworks_proof.bin
