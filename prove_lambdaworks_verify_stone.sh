75c8f237cb8606bd9785f9a6ddbbf3c30263f2b2
MauroFab <maurotoscano2@gmail.com>
2023-10-27 18:30:04 -0300

    Add text

--- prove_lambdaworks_verify_stone.sh
+++ prove_lambdaworks_verify_stone.sh
@@ -2,7 +2,10 @@
 
 # Prove a Fibonacci example using Lambdaworks. This produces a file `lambdaworks_proof.bin`
 # containing the proof in the format expected by Stone.
+echo -e "Generating proof with Lambdaworks Stark Platinum ...\n"
 /app/lambdaworks_prover/target/release/lambdaworks_prover
+echo -e "Proof generated!"
 
+echo -e "Verifying proof with Stone ...\n"
 # Read proof from file `lambdaworks_proof.bin` and verify it using Stone.
 /app/build/Release/fibonacci_air_test_cases/verify_from_bytes lambdaworks_proof.bin
