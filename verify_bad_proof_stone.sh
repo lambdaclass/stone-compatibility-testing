41b401ea663b799606d56aae0be20dda5441b607
MauroFab <maurotoscano2@gmail.com>
2023-10-27 18:33:18 -0300

    Style

--- verify_bad_proof_stone.sh
+++ verify_bad_proof_stone.sh
@@ -3,9 +3,9 @@
 # Prove a Fibonacci example using Lambdaworks. This produces a file `lambdaworks_proof.bin`
 # containing the proof in the format expected by Stone.
 
-echo -e "Generating an invalid proof\n"
+echo -e "Creating an invalid proof\n"
 printf "%b" '\xff\xff\xff\xff' > fake_proof.bin
 
 # Read proof from file `lambdaworks_proof.bin` and verify it using Stone.
-echo -e "Verifying with stone \n"
+echo -e "Verifying proof with Stone ..."
 /app/build/Release/fibonacci_air_test_cases/verify_from_bytes fake_proof.bin
